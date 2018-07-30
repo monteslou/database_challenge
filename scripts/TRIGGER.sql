106c8330a467bd205dabd5c2416c3f9ef1df3e6d
Juan David <david.rodriguez@endava.com>
2018-07-30 17:25:47 -0500

    included backup scripts

--- scripts/TRIGGER.sql
+++ scripts/TRIGGER.sql
@@ -1,24 +1,22 @@
 CREATE OR REPLACE FUNCTION process_emp_audit() RETURNS TRIGGER AS $emp_audit$
     BEGIN
-	IF OLD.AGE != NEW.AGE THEN
 		IF NEW.AGE%2 = 1 THEN
-            DECLARE NEW.AGE := NEW.AGE-5
+            NEW.AGE := NEW.AGE-5;
 		ELSE
-			 SET NEW.AGE = NEW.AGE+3
+			 NEW.AGE := NEW.AGE+3;
         END IF;
-	END IF;
-		
-        --INSERT INTO AUDIT_tbl(STAMP, age) VALUES (NOW(), new.age);
-        RETURN NULL; -- result is ignored since this is an AFTER trigger
+		RETURN NEW;
     END;
 $emp_audit$ LANGUAGE plpgsql;
 
-DROP TRIGGER AUDIT_TRG ON TEST2
+DROP TRIGGER AUDIT_TRG ON TEST2;
 CREATE TRIGGER AUDIT_trg
     BEFORE UPDATE ON TEST2
     FOR EACH ROW
-    --WHEN (OLD.balance IS DISTINCT FROM NEW.balance)
+    WHEN (OLD.age IS DISTINCT FROM NEW.age)
     EXECUTE PROCEDURE process_emp_audit();
 	
 	INSERT INTO TEST2(YN, AGE, BOOL, CITY, DOLLAR)
-VALUES('Y', 78, true, 'bogota', '34.00'), ('Y', 45, false, 'chia', '340.00'),('N', 13, true, 'cajica', '84.00')
+VALUES('Y', 78, true, 'bogota', '34.00'), ('Y', 45, false, 'chia', '340.00'),('N', 13, true, 'cajica', '84.00');
+
+UPDATE TEST2 SET AGE=39 WHERE id=999999;
